768207d5267efe09795c315feed1ca1cdb942728
Alejandro Mdz <alejandromdz@gmail.com>
2017-02-27 01:16:30 -0800

    added angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('sass', function () {
   return gulp.src('./src/**/*.scss')
@@ -7,4 +9,10 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default',['sass']);
+gulp.task('scripts', function() {
+    var tsResult = gulp.src("./src/**/*.ts") 
+        .pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default',['sass','scripts']);
