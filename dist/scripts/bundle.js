2a075f38bf311c8131bf2520ee6c29d144de268a
Alejandro Mdz <alejandromdz@gmail.com>
2017-03-15 12:20:58 -0700

    slide effect implemented

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = require("./config");
 var controllers_1 = require("./controllers");
-angular.module('myApp', ['ui.router'])
+angular.module('myApp', ['ui.router', 'ngAnimate'])
     .config(config_1.default)
     .controller('MainController', controllers_1.default);
 
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,6 +1,6 @@
 import Config from './config';
 import MainController from './controllers';
 
-angular.module('myApp',['ui.router'])
+angular.module('myApp',['ui.router','ngAnimate'])
 .config(Config)
 .controller('MainController',MainController);
